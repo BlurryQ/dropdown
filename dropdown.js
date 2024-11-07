555bc9a85cba847bc4fada71c81a4ab7e536a946
BlurryQ <jazz.barlow@outlook.com>
2024-11-07 17:18:50 +0000

    Corrects intial click

--- dropdown.js
+++ dropdown.js
@@ -1,31 +1,38 @@
-const dropdowns = document.querySelectorAll('.dropdown')
+const selects = document.querySelectorAll('.select')
 
-dropdowns.forEach(dropdown => {
+selects.forEach(select => {
     const selected = document.querySelector('.selected')
     const menu = document.querySelector('.menu')
 
-    dropdown.addEventListener('click', (e) => {
+    select.addEventListener('click', (e) => {
         menu.classList.toggle('menu-open')
 
-        menuLis = document.querySelectorAll('.menu li')
-        menuLis.forEach(li => {
-            li.addEventListener('click', (e) => {
-
-                const selection = e.target.innerText
-                console.log(selection, 'clicked!!')
-                console.log(" ")
-                selected.innerText = selection
-                menu.classList.toggle('menu-open')
-
-                menuLis.forEach(lis => {
-                    lis.classList.remove('active')
-                })
-                li.classList.toggle('active')
-            })
-        })
+        // if not inside our element close menu
     })
 
+    document.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape') {
+            menu.classList.remove('menu-open')   
+        }
+    })
+
+    menuLis = document.querySelectorAll('.menu li')
+    menuLis.forEach(li => {
+        li.addEventListener('click', (e) => {
+            const selection = e.target.innerText
+            console.log(selection, 'clicked!!')
+            console.log(" ")
+            selected.innerText = selection
+            menu.classList.remove('menu-open')
+            menuLis.forEach(lis => {
+                lis.classList.remove('active')
+            })
+            li.classList.toggle('active')
+        })
+    })
 })
+
+
 
 
 
